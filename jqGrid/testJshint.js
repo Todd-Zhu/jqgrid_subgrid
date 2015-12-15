a229fed0cbf7d6f46aec145bdcd044b7d1f3f923
zhulf <zhulf@shishike.com>
2015-12-15 10:45:15 +0800

    jqGrid显示子报表栗子

---
+++ jqGrid/testJshint.js
@@ -0,0 +1,13 @@
+var arr = [1,2,3];
+for (var i = 0; i < arr.length; i++) {
+	var b = arr[i];
+	console.log(parseInt(b));
+	for (var i = 0; i < arr.length; i++) {
+	var b = arr[i];
+	console.log(parseInt(b));
+}
+}
+for (var i = 0; i < arr.length; i++) {
+	var b = arr[i];
+	console.log(parseInt(b));
+}
